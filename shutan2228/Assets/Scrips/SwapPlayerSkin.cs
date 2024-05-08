85e6124c035a44627f989a1d193a5af48183bb22
kilopopik8 <168898865+kilopopik8@users.noreply.github.com>
2024-05-08 16:33:10 +0300

    version5
    добавил все спрайты скинов из ассета + добавил пару спрайтов энеми для этого уровня и анимация бега для них коллайдер на большенстве выключил ибо нехуй порисуй скины пж)

--- shutan2228/Assets/Scrips/SwapPlayerSkin.cs
+++ shutan2228/Assets/Scrips/SwapPlayerSkin.cs
@@ -6,52 +6,14 @@ public class SwapPlayerSkin : MonoBehaviour
 {
     private Animator animator;
 
-    public AnimationClip newIdleAnimation1;
-    public AnimationClip newIdleAnimation2;
-    public AnimationClip newIdleAnimation3;
-    public AnimationClip newIdleAnimation4;
-    public AnimationClip newIdleAnimation5;
-    public AnimationClip newIdleAnimation6;
-    public AnimationClip newIdleAnimation7;
-    public AnimationClip newIdleAnimation8;
-    public AnimationClip newIdleAnimation9;
+    public AnimationClip[] newIdleAnimation;
+
     public int index;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        if (index==0) 
-        {
-            animator.Play(newIdleAnimation1.name);
-        }
-        if (index==1) 
-        {
-            animator.Play(newIdleAnimation2.name);
-        }
-        if (index==2) 
-        {
-            animator.Play(newIdleAnimation3.name);
-        }
-        if (index==3)
-        {
-            animator.Play(newIdleAnimation4.name);
-        }
-        if (index==4) 
-        {
-            animator.Play(newIdleAnimation5.name);
-        }
-        if (index==5) 
-        {
-            animator.Play(newIdleAnimation6.name);
-        }
-        if (index==6) 
-        {
-            animator.Play(newIdleAnimation7.name);
-        }
-        if (index==7) 
-        {
-            animator.Play(newIdleAnimation8.name);
-        }
+        animator.Play(newIdleAnimation[index].name);
     }
 
 }
